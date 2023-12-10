e22697100ff1da540e8ed67b0ce0a4846f067d85
jteich99 <jteich@fi.uba.ar>
2023-12-10 12:54:27 -0300

    algunas modificaciones

--- .config/rofi/scripts/powermenu.sh
+++ .config/rofi/scripts/powermenu.sh
@@ -34,11 +34,11 @@ uptime=$(uptime -p | sed -e 's/up //g')
 rofi_command="rofi -theme $dir/$theme"
 
 # Options
-shutdown=""
-reboot=""
-lock=" "
-suspend=""
-logout=""
+shutdown="shutdown"
+reboot="reboot"
+lock="lock"
+suspend="suspend"
+logout="logout"
 
 # Confirmation
 confirm_exit() {
