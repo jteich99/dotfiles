9d20fe4d9e318f0a4f5606a27e582ca7ce0b60d1
jteich99 <jteich@fi.uba.ar>
2024-02-29 12:02:12 -0300

    nvim: add obsidina plugin

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -1,1 +1,37 @@
+return {
+    "epwalsh/obsidian.nvim",
+    version = "*",  -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+    --   "BufReadPre path/to/my-vault/**.md",
+    --   "BufNewFile path/to/my-vault/**.md",
+    -- },
     dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    opts = {
+    },
+    config = function()
+        vim.opt_local.conceallevel = 1
+        require("obsidian").setup({
+            workspaces = {
+                {
+                    name = "juani-sync",
+                    path = "~/Documents/juani-sync",
+                },
+            },
+            preferred_link_style = "wiki",
+        })
+        vim.keymap.set("n", "gf", function()
+            if require("obsidian").util.cursor_on_markdown_link() then
+                return "<cmd>ObsidianFollowLink<CR>"
+            else
+                return "gf"
+            end
+        end, { noremap = false, expr = true })
+    end
+}
