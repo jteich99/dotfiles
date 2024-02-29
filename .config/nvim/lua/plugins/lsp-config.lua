8c7375ab66a8e655af10eff81e791077cfcb3583
jteich99 <jteich@fi.uba.ar>
2024-02-29 11:56:43 -0300

    nvim: update lsp-config

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -14,7 +14,7 @@ return {
                     "foam_ls",
                     "clangd",
                     "texlab",
-                    "ltex",
+                    -- "ltex",
                     -- comento los LSP de c++ y de openFOAM porque no funcionan bien cuando estoy desarrollando actuationDisks
                     -- "pyright",
                     -- "jedi_language_server",
@@ -31,27 +31,12 @@ return {
             lspconfig.foam_ls.setup {}
             lspconfig.clangd.setup {}
             lspconfig.texlab.setup {}
-            lspconfig.ltex.setup {}
+            -- lspconfig.ltex.setup {}
             -- lspconfig.pyright.setup {}
             -- lspconfig.jedi_language_server.setup {}
             lspconfig.pylsp.setup {}
 
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-
             vim.diagnostic.disable()
-            local diagnostics_active = false 
-            vim.keymap.set('n', '<leader>d', function()
-                diagnostics_active = not diagnostics_active
-                if diagnostics_active then
-                    vim.diagnostic.show()
-                    vim.diagnostic.enable()
-                else
-                    vim.diagnostic.hide()
-                    vim.diagnostic.disable()
-                end
-            end)
         end
     }
 }
