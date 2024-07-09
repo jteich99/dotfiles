0c0f4ef58e57a412c4e8747b3baeeafc457592d0
jteich99 <jteich@fi.uba.ar>
2024-07-08 22:38:23 -0300

    nvim: configure barbar

--- .config/nvim/lua/plugins/barbar.lua
+++ .config/nvim/lua/plugins/barbar.lua
@@ -1,21 +1,27 @@
 return {
     "romgrk/barbar.nvim",
+    dependencies = {
+        'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+        'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    },
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
     config = function()
-        require('lazy').setup {
-            {
-                'romgrk/barbar.nvim',
-                dependencies = {
-                    'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
-                    'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+        vim.g.barbar_auto_setup = false 
+        require('barbar').setup{
+            highlight_visible = true, 
+            insert_at_end = true,
+            icons = {
+                separator = {left = '▎', right = ''},
+                separator_at_end = true,
+            },
+            sidebar_filetypes = {
+                NvimTree = true,
+                undotree = {
+                    text = 'undotree',
+                    align = 'center',
                 },
-                init = function() vim.g.barbar_auto_setup = false end,
-                opts = {
-                  -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-                  -- animation = true,
-                  -- insert_at_start = true,
-                  -- …etc.
-                },
-                version = '^1.0.0', -- optional: only update when a new 1.x version is released
+            ['neo-tree'] = {event = 'BufWipeout'},
+            Outline = {event = 'BufWinLeave', text = 'symbols-outline', align = 'right'},
             },
         }
     end
