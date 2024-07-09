978f20d01c10b7641f6da73df0830dbbc8e31297
jteich99 <jteich@fi.uba.ar>
2024-07-08 22:39:02 -0300

    nvim: add plugins for grammar check

---
+++ .config/nvim/lua/plugins/grammarous.lua
@@ -0,0 +1,11 @@
+return {
+    -- "rhysd/vim-grammarous",
+    -- config = function()
+    --     vim.g.grammarous_jar_url = 'https://www.languagetool.org/download/LanguageTool-5.9.zip'
+    --     vim.g.grammarous_java_dir = '~/.local/share/nvim/lazy/vim-grammarous/misc/LanguageTool-5.9'
+
+    --     require('LanguageTool').setup{
+    --         autocmd Filetype tex LanguageToolSetUp
+    --     }
+    -- end
+}
---
+++ .config/nvim/lua/plugins/languageTool.lua
@@ -0,0 +1,9 @@
+return {
+    -- "vigoux/LanguageTool.nvim",
+    -- config = function()
+
+    --     require('LanguageTool').setup{
+    --         autocmd Filetype tex LanguageToolSetUp
+    --     }
+    -- end
+}
