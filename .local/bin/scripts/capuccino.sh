bea96fb6952593ff8f3cad639b1aacd94c84f37f
jteich99 <jteich@fi.uba.ar>
2024-09-02 11:25:00 -0300

    clean up

--- .local/bin/scripts/capuccino.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
- 
-## script to toggle screen lock inhibition
- 
-if [ -f "/home/$USER/.config/openbox/cappuccino" ];
-    then rm -f "/home/$USER/.config/openbox/cappuccino" && notify-send "Screen saver enabled!"
-    else touch "/home/$USER/.config/openbox/cappuccino" && notify-send "Screen saver disabled!"
-fi
